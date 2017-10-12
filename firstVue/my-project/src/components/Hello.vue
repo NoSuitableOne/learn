1725d2ec0365035df73d54854e292d655f744be7
ma <210181238@qq.com>
2017-10-13 07:42:50 +0800

    add vue

---
+++ firstVue/my-project/src/components/Hello.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+    <h2>More things</h2>
+    <ul>
+      <li v-for="item in set">
+        {{item.key}}: {{item.version}}
+      </li>
+    </ul>
+    <div>
+      <input class="into" type="text" v-model="input_val">
+      Input value: <span v-text="input_val"></span>
+    </div>
+    <div>
+      <button v-on:click="times++" class="btn btn-primary">click on me {{times}} times!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'I am VUE',
+      set: [
+        {
+          key: 'Angular',
+          version: 4
+        },
+        {
+          key: 'Vue',
+          version: 2
+        },
+        {
+          key: 'React',
+          version: 15.6
+        }
+      ],
+      input_val: '',
+      times: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.into {
+  display: block;
+  margin: 0 auto;
+}
+</style>
