ccc2b00adc1011d3cd67003312990dd93b035730
chenlijuan <chenlijuan@souche.com>
2020-05-10 22:34:37 +0800

    待改进promise任务链

--- basic/js/promiseA.js
+++ basic/js/promiseA.js
@@ -24,20 +24,87 @@ function promiseCancelableWrapper (p) {
     }
   };
 };
-let eg = new Promise(( resolve, reject ) => {
-  let number = Math.random();
-  if (number > 0.5) {
-    resolve(number);
-  } else {
-    reject(number);
+
+class Plan {
+  constructor(queue, state) {
+    this.queue = queue || [];
+    this.state = state || {};
   }
+
+  before (task) {
+    this.queue.unshift(task);
+  }
+
+  after (task) {
+    this.queue.push(task);
+  }
+
+  setState = (value) => {
+    this.state = {
+      ...this.state,
+      ...value
+    };
+  }
+
+  getState = () => {
+    return this.state;
+  };
+
+  eachCall = (lastPromise, next) => {
+    lastPromise
+    .then(value => {
+      this.setState(value);
+    })
+    .catch(error => {
+      console.error('error:', error);
+    });
+    return next.fn(...next.params);
+  }
+
+  execute () {
+    return this.queue.reduce(
+      (lastPromise, ele) => this.eachCall(lastPromise, ele), 
+      new Promise(resolve => resolve())
+    );
+  }
+}
+
+let number = Math.random();
+
+function eg (number, therehold = 0.5, name, time = 2) {
+  return new Promise(( resolve, reject ) => {
+    setTimeout(() => {
+      if (number > therehold) {
+        resolve({ [name]: number });
+      } else {
+        reject(number);
+      }
+    }, time * 1000);
+  });
+}
+/* cancel promise */
+// let canceledP = promiseCancelableWrapper(eg(number, 0.2));
+// canceledP.cancel();
+
+/* async tasks */
+let planA = new Plan([
+  { 
+    fn: eg,
+    params: [ number, 0, 'eg1', 10 ]
+  }, 
+  {
+    fn: eg,
+    params: [ number, 0, 'eg2' ]  
+  },
+  {
+    fn: eg,
+    params: [ number, 0, 'eg3' ]
+  },
+  {
+    fn: eg,
+    params: [ number, 0, 'eg4' ]
+  }
+]);
+planA.execute().then(()=>{
+  console.log(planA.getState());
 });
-
-let canceledP = promiseCancelableWrapper(eg);
-canceledP.cancel();
-
-console.log(
-  canceledP.promise
-    .then(v => console.log('then: ', v))
-    .catch(err => console.error('error:', err))
-);
